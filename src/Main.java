6559f8b315da4b7d539b45d57c5eed62add70ce5
olensis <olensis7gmail@.com>
2022-08-26 17:51:18 +0300

    первый коммит.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         var homeWork3 = 3;
-        System.out.println( "Домашнее задание номер" + homeWork3);
+        System.out.println( "Домашнее задание номер" + homeWork3 );
 
 
     }
