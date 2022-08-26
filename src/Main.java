cfb01fd843137017952f98db7769d630e8b80525
olensis <olensis7gmail@.com>
2022-08-26 17:54:15 +0300

    второй коммит ( добавила ветку)

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
         var homeWork3 = 3;
         System.out.println( "Домашнее задание номер" + homeWork3 );
+        var newBranch = 3;
+
 
 
     }
